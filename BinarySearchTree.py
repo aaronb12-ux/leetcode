fca1db7e0067a50cfbd951305a79f93206a860cc
Aaron <afairster03@gmail.com>
2025-11-29 22:05:02 -0800

    Update BinarySearchTree.py

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -1,3 +1,12 @@
+class TreeNode:
+
+    def __init__(self, left=None, right=None, val=0):
+        self.left = left
+        self.right = right
+        self.val = val
+
+
+
 class BinarySearchTrees:
 
     def inOrder(self, root): #get nodes in SORTED order
@@ -73,3 +82,67 @@ class BinarySearchTrees:
             ans += self.rangeSum(root.left, low, high)
         
         return ans
+    
+
+
+    def validBinarySearchTree(self, root): #given a tree, determine if it is a binary search tree
+
+        def isSorted(nodesList: list) -> bool:
+
+            left = 0
+            right = 1
+
+            while right < len(nodesList):
+
+                if nodesList[left] >= nodesList[right]:
+                    return False
+                
+                left += 1
+                right += 1
+            
+            return True
+        
+        def inOrder(node, nodesList: list) -> list:
+
+            if not node:
+                return
+            
+            inOrder(node.left, nodesList)
+            nodesList.append(node.val)
+            inOrder(node.right, nodesList)
+
+            return nodesList
+    
+        nodesList = inOrder(root, [])
+
+        return isSorted(nodesList)
+
+
+    def insertNewNode(self, root, val):
+
+        def traverseTree(node, val):
+
+            if not node:
+                newNode = TreeNode(val=val)
+                return newNode
+
+            if node.val < val:
+                node.right = traverseTree(node.right, val)
+            if node.val > val:
+                node.left = traverseTree(node.left, val)
+
+            return node
+        
+
+        root = traverseTree(root, val)
+
+        return root
+
+
+
+
+
+
+
+
+
