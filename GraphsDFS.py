e94b6a4ef8f7039c20287b8e47c686483d062d2b
Aaron <afairster03@gmail.com>
2025-12-04 15:27:00 -0800

    Create GraphsDFS.py

---
+++ GraphsDFS.py
@@ -0,0 +1,64 @@
+from collections import defaultdict
+class dfsGRAPHS:
+
+    def numberOfProvinces(self, isConnected: List[List[int]]) -> int:
+
+        def makeAdjList():
+
+            n = len(isConnected)
+            adjList = defaultdict(list)
+
+            for i in range(n):
+                for j in range(n):
+                    if isConnected[i][j] == 1:
+                        adjList[i + 1].append(j + 1)
+            
+            return adjList
+        
+        def dfs(node):
+            for neighbor in adjList[node]:
+                if neighbor not in seen:
+                    seen.add(neighbor)
+                    dfs(neighbor)
+
+        adjList = makeAdjList()
+        seen = set()
+        ans = 0
+
+        for node in adjList:
+            if node not in seen:
+                ans += 1
+                seen.add(node)
+                dfs(node)
+        
+        return ans
+    
+    def numberOfIslands(self, grid: List[List[int]]) -> int:
+
+        def isValid(row, col):
+            return 0 <= row <= numRows and 0 <= col <= numCols
+        
+        def dfs(row, col):
+
+            for dy, dx in directions:
+                rowChange = row + dy
+                colChange = col + dx
+
+                if isValid(rowChange, colChange):
+                    if (rowChange, colChange) not in seen and grid[rowChange][colChange] == "1":
+                        seen.add((rowChange, colChange))
+                        dfs(rowChange, colChange)
+                        
+        numRows = len(grid)
+        numCols = len(grid[0])
+        directions = [(1,0),(-1,0),(0,-1),(0,1)] #down, up, right, left
+        seen = set()
+        ans = 0
+
+        for row in range(numRows):
+            for col in range(numCols):
+                if grid[row][col] == "1" and (row, col) not in seen:
+                    seen.add((row, col))
+                    ans += 1
+                    dfs(row, col)
+        return ans
