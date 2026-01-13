1dc8e6ce5de0a3deef00af87b2bafc06b56df9d9
Aaron <afairster03@gmail.com>
2026-01-12 21:54:25 -0800

    Update blind75arraysandhashing.py

--- blind75arraysandhashing.py
+++ blind75arraysandhashing.py
@@ -1,1 +1,21 @@
+def productExceptSelf(self, nums: List[int]) -> List[int]:
 
+        ans = []
+
+        for i in range(len(nums)):
+            curr = 1
+
+            if i == len(nums) - 1: #last element
+                allNums = nums[:i] #everything excluding the last element
+
+            else:
+                first_half = nums[:i]
+                second_half = nums[i + 1:]
+                allNums = first_half + second_half
+
+            for num in allNums:
+                curr = curr * num
+
+            ans.append(curr)
+    
+        return ans
