5aea1ef08cec31a63807f370a0f36580d9c588e6
Aaron <afairster03@gmail.com>
2026-02-09 04:32:57 -0800

    Update blind75slidingwindow.py

--- blind75slidingwindow.py
+++ blind75slidingwindow.py
@@ -14,3 +14,32 @@ class Solution:
         
         return ans
 
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        left = 0
+        right = 0
+        currentWindow = set()
+        ans = 0
+
+
+        if len(s) == 1:
+            return 1
+
+        while left < len(s) and right < len(s):
+    
+            if s[right] not in currentWindow:
+                currentWindow.add(s[right])
+                ans = max(ans, right - left + 1)
+                right += 1
+                print(ans)
+               
+            else:
+                while s[right] in currentWindow:
+                    currentWindow.remove(s[left])
+                    left += 1
+                   
+
+        return ans
+
