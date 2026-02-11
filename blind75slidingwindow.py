96454ba786b4fb22e402756314096f130b11d113
Aaron <afairster03@gmail.com>
2026-02-10 20:29:34 -0800

    Update blind75slidingwindow.py

--- blind75slidingwindow.py
+++ blind75slidingwindow.py
@@ -43,3 +43,45 @@ class Solution:
 
         return ans
 
+
+from collections import defaultdict
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+
+        def mostFrequent():
+
+            maxFreq = 0
+
+            for key in currentWindow:
+
+                maxFreq = max(maxFreq, currentWindow[key])
+
+            return maxFreq
+
+        left = 0
+        currentWindow = defaultdict(int)
+        ans = 0
+
+        for right in range(len(s)):
+
+            currentWindow[s[right]] += 1
+
+            windowSize = right - left + 1
+            
+            if windowSize - mostFrequent() <= k: #valid window
+
+                ans = max(windowSize, ans)
+            
+            while (right - left + 1) - mostFrequent() > k:
+       
+                currentWindow[s[left]] -= 1
+
+                left += 1
+            
+            if right == len(s) - 1:
+                
+                return max(ans, right - left + 1)
+            
+        return ans
+
