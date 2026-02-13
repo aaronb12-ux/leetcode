98428ecec662c3e9149312bfd3ec8e54ed6e68e1
Aaron <afairster03@gmail.com>
2026-02-13 01:28:34 -0800

    Update blind75binsearch.py

--- blind75binsearch.py
+++ blind75binsearch.py
@@ -24,3 +24,50 @@ class Solution:
                 right = mid
         
         return nums[left]
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+
+        '''
+        binary search -> 
+        what should the condition be that makes us update pointers
+
+        '''
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+
+                return mid
+
+
+            if nums[left] <= nums[mid]:
+
+                #mid is in left portion
+                if target > nums[mid] or target < nums[left]:
+
+                    #search right
+                    left = mid + 1
+
+                else:
+
+                    right = mid - 1
+
+            else: #mid is in the right portion
+
+                if target < nums[mid] or target > nums[right]:
+
+                    right = mid - 1
+                
+                else:
+
+                    left = mid + 1 
+            
+        return -1
