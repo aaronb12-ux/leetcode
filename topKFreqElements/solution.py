e18c7c55a3dfb6966de3f1a790de0762e5f68c1d
Aaron <afairster03@gmail.com>
2026-07-25 12:24:04 -0700

    Create solution.py

---
+++ topKFreqElements/solution.py
@@ -0,0 +1,17 @@
+from heapq import *
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        counts = Counter(nums)
+
+        heap = [] 
+
+        for key, val in counts.items():
+            heappush(heap, (val, key))
+
+            if len(heap) > k:
+                heappop(heap)
+        
+        return [pair[1] for pair in heap]
+            
